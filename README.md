03187bbbbbe144c27941e15644522476611cee0b
Newton <narmstrongv@gmail.com>
2021-05-04 18:44:06 -0700

    README updated/added js M1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Password Generator Starter Code
+## Password Generator Starter Code
+
+# Description
+* The Password Generator creates random passwords. It allows the user to enter their criteria for passwords (i.e. lowercase, uppercase, numeric, special characters) and then create said password. The password will be between 8 - 128 characters.
+
+# Screenshot
+
+# Link
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 // Assignment code here
 
+//Pass-Generator-Method 1
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
